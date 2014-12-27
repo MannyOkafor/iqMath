c83ac4c3231b9aaa9c72eb99e34e0c5452713fa2
Orens Xhagolli <orens.xhagolli@hotmail.com>
2014-12-26 23:54:22 -0500

    Bad input checks and solves non-operational input (e.g. solving 322)

--- evaluate.py
+++ evaluate.py
@@ -4,7 +4,22 @@
     description: Evaluates a expression
 """
 
+import iqMath
 
 
 def main():
-    response = input("Please input the expression to evaluate: ")
+    expression = input("Please input the expression to evaluate: ")
+
+
+def evaluate(string):
+    operations = list("+-*/")
+    breakable = False
+    for item in operations:
+        if item in string:
+            breakable = True
+    for char in string[0:3]:
+        if char.isDigit():
+            breakable = True
+
+    if breakable == False:
+        return string
--- iqMath.py
+++ iqMath.py
@@ -9,14 +9,16 @@ import evaluate
 #import os #Optional if you want to use commands like "clear()"
 
 
-def main():
+def main(param = None):
     print("Welcome to iqMath!")
     print("Please choose what would you like to do next.")
     print("1. Evaluate an expression.")
 
+    if param is not None: #Bad input case
+        print ("Please enter an integer from the above list.")
+        
     response = int(input("Enter number: ").strip())
     if response == 1:
         evaluate.main() #transfers control to the other file
     else:
-        clear()
-        main()
+        main(1) #bad input

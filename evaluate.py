9339d87b8ab8911fd44af711812da5029d7720a6
Orens Xhagolli <orens.xhagolli@hotmail.com>
2014-12-26 23:37:33 -0500

    Set up evaluate and control transfer between modules

---
+++ evaluate.py
@@ -0,0 +1,10 @@
+"""
+    file: evaluate.py
+    author: Orens Xhagolli
+    description: Evaluates a expression
+"""
+
+
+
+def main():
+    response = input("Please input the expression to evaluate: ")
--- iqMath.py
+++ iqMath.py
@@ -5,7 +5,18 @@
     I'm still not sure how far to take this)
 """
 
+import evaluate
+#import os #Optional if you want to use commands like "clear()"
+
+
 def main():
-    print("Welcome to iqMath")
+    print("Welcome to iqMath!")
     print("Please choose what would you like to do next.")
-    
+    print("1. Evaluate an expression.")
+
+    response = int(input("Enter number: ").strip())
+    if response == 1:
+        evaluate.main() #transfers control to the other file
+    else:
+        clear()
+        main()

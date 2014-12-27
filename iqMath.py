224f2e5e61b1ac499580e006725b65cc4cb05a7d
Orens Xhagolli <orens.xhagolli@hotmail.com>
2014-12-26 23:17:09 -0500

    Switched to python due to the easiness of working with strings

---
+++ iqMath.py
@@ -0,0 +1,11 @@
+"""
+    file: iqMath.py
+    author: Orens Xhagolli
+    description: This does math! (Sorry for general description,
+    I'm still not sure how far to take this)
+"""
+
+def main():
+    print("Welcome to iqMath")
+    print("Please choose what would you like to do next.")
+    

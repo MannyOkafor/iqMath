c20a8f58a8bd57f36f5adf30a6170522456f9f77
Orens Xhagolli <orens.xhagolli@hotmail.com>
2014-12-27 21:58:19 -0500

    Finished operation checking, back() function to move to the main menu

--- evaluate.py
+++ evaluate.py
@@ -9,17 +9,32 @@ import iqMath
 
 def main():
     expression = input("Please input the expression to evaluate: ")
+    evaluate(expression)
 
 
 def evaluate(string):
+    
+    if string == "back()": #Go back
+        iqMath.main()
+        
+    if complete(string): #Check if there are empty 
+        return string
+
+    peval(string) #scans for parentheses
+
+
+def peval(string):
+    pass
+
+
+def complete(string):
     operations = list("+-*/")
     breakable = False
-    for item in operations:
+    for item in operations: #Are there unperformed operations?
         if item in string:
             breakable = True
-    for char in string[0:3]:
+    for char in string[0:3]: #Are there any functions being performed?
         if char.isDigit():
             breakable = True
 
-    if breakable == False:
-        return string
+    return not breakable

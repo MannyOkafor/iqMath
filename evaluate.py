4e8de44a59333a5d6a83d44c61a1632a5379d89b
Orens Xhagolli <orens.xhagolli@hotmail.com>
2014-12-29 17:58:36 -0500

    Added all basic operations, detected problem when there is more than one parenthese

--- evaluate.py
+++ evaluate.py
@@ -8,8 +8,10 @@ import iqMath
 
 
 def main():
-    expression = input("Please input the expression to evaluate: ")
-    print(evaluate(expression))
+    expression = 0
+    while expression != "exit()":
+        expression = input("Please input the expression to evaluate: ")
+        print(evaluate(expression))
 
 
 def evaluate(string):
@@ -24,21 +26,71 @@ def evaluate(string):
 
     string = peval(string) #scans for parentheses and evaluates them
     string = md(string)
+    string = addsub(string)
     return string
+
+
+def addsub(string):
+    if "+" in string or "-" in string:
+        arg1 = ""
+        arg2 = ""
+        oper = None
+        i = 0
+        while i < len(string):
+            if string[i].isdigit() or string[i] == ".":
+                if oper is None:
+                    arg1 += string[i]
+                else:
+                    arg2 += string[i]
+            else:
+                if oper is None:
+                    oper = string[i]
+                else:
+                    break
+            i+=1
+        if oper == "+":
+            return evaluate(str(float(arg1)+float(arg2))+string[i:])
+        elif oper == "-":
+            return evaluate(str(float(arg1)-float(arg2))+string[i:])
+    else:
+        return string
 
 
 def md(string):
     if "*" in string or "/" in string:
-        mindex = len(string)
-        dindex = len(string)
-        if "*" in string:
-            mindex = string.index("*")
-        if "/" in string:
-            dindex = string.index("/")
-        if mindex < dindex:
-            return str(float(evaluate(string[:mindex]))*float(evaluate(string[mindex+1:])))
+        arg1 = ""
+        arg2 = ""
+        a1index = 0
+        a2index = 0
+        oper = None
+        for i in range(0,len(string)):
+            if string[i].isdigit() or string[i] == ".":
+                if oper is None:
+                    arg1 += string[i]
+                else:
+                    arg2 += string[i]
+                    a2index = i
+            elif string[i] == "+" or string[i] == "-":
+                if oper is None:
+                    arg1 = ""
+                    a1index = i+1
+                else:
+                    break
+            else:
+                if oper is None:
+                    oper = string[i]
+                else:
+                    break
+        if oper == "*":
+            temp = evaluate(string[:a1index]+str(float(arg1) * float(arg2))+string[a2index:])
+            return temp[:len(temp)-1] #-1 corrects for floating point arithmetic errors
         else:
-            return str(float(evaluate(string[:dindex]))/float(evaluate(string[dindex+1:])))
+            if float(arg2) == 0.0:
+                print("Error: Cannot divide by 0")
+                main()
+                return
+            temp = evaluate(string[:a1index]+str(float(arg1) / float(arg2))+string[a2index:])
+            return temp[:len(temp)-1]
     else:
         return string
 
@@ -47,7 +99,11 @@ def peval(string):
     if "(" in string:
         if ")" in string[::-1]:
             temp = string[string.index('(')+1:len(string) - string[::-1].index(')')-1]
-            return string[0:string.index('(')] + evaluate (temp) + string[len(string) - string[::-1].index(')')+1:]
+            cmplstr = string[:string.index(temp)-1]+evaluate(temp)+string[string.index(temp)+len(temp)+1:]
+            if "(" in cmplstr:
+                peval(cmplstr)
+            else:
+                return cmplstr
         else:
             print("Error: Did not enclose parentheses. Try again!")
             main()
@@ -66,7 +122,8 @@ def complete(string):
     for item in operations: #Are there unperformed operations?
         if item in string:
             literal = False
-    if not string[0].isdigit(): #Are there functions or uresolved variables?
-        literal = False
+    if string is not None or string != "":
+        if not string[0].isdigit(): #Are there functions or uresolved variables?
+            literal = False
 
     return literal

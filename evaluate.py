0591eab9bf83eb0e9336696273452b20746cc08e
Orens Xhagolli <orens.xhagolli@hotmail.com>
2014-12-28 20:40:13 -0500

    Handles single M/D operations

--- evaluate.py
+++ evaluate.py
@@ -23,7 +23,28 @@ def evaluate(string):
         return string
 
     string = peval(string) #scans for parentheses and evaluates them
+    string = md(string)
     return string
+
+
+def md(string):
+    if "*" in string or "/" in string:
+        mindex = -1
+        dindex = -1
+        if "*" in string:
+            mindex = string.index("*")
+        if "/" in string:
+            dindex = string.index("/")
+        if mindex == -1:
+            if float(evaluate(string[dindex+1:])) == 0.0:
+                print("Error: Cannot divide by 0")
+                main()
+                return
+            return str(float(evaluate(string[:dindex]))/float(evaluate(string[dindex+1:])))
+        if dindex == -1:
+            return str(float(evaluate(string[:mindex]))*float(evaluate(string[mindex+1:])))
+    else:
+        return string
 
 
 def peval(string):
